5c0e0852a9ead90eb441fe10ee20580e03844326
gabriel vitor <gabrielvitortec@gmail.com>
2021-11-26 14:47:34 -0300

    exercicios de python

---
+++ Curso Python/Desafios/desafio003aula06.py
@@ -0,0 +1,7 @@
+
+n1 = int(input("digite um valor: "))
+n2 = int(input("digite um valor: "))
+
+soma = n1 + n2
+
+print("a soma é", soma)
---
+++ Curso Python/Desafios/desafio004aula06.py
@@ -0,0 +1,10 @@
+
+n = input('digite algo: ')
+
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isupper())
+print(n.islower())
+print(n.isdecimal())
+print(n.isalnum())
+print(type(n))
