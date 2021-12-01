edd688375beddf17cceec4d6c060da1369391672
gabriel vitor <gabrielvitortec@gmail.com>
2021-12-01 16:10:31 -0300

    tarefa 1 estruturas de repetição

---
+++ Curso Python/Tarefas/tarefa1_estruturas_de_repeticao.py
@@ -0,0 +1,15 @@
+somaidade = 0
+
+n = int(input("informe a quantidade de pessoas da turma"))
+
+for c in range(1, n + 1):
+    idade = int(input("aluno {} - informe sua idade: ".format(c)))
+    somaidade = somaidade + idade
+
+media = somaidade / n
+if media <= 25:
+    print("Turma jovem")
+elif media < 60:
+    print("turma adulta")
+else:
+    print("turma idosa")
