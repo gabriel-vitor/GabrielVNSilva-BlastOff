545816f36b49079d3098edeb8b1fab6392032d8b
gabriel vitor <gabrielvitortec@gmail.com>
2021-12-08 14:29:44 -0300

    tarefa2 - funções conclúído

--- Curso Python/Tarefas/tarefa2_funcoes.py
+++ Curso Python/Tarefas/tarefa2_funcoes.py
@@ -6,16 +6,24 @@ meses = [0, 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
 def validate(date_text):
     try:
         datetime.datetime.strptime(date_text, '%d/%m/%Y')
+        #strptime não verifica se o digito é decimal
+        print('Formatação correta.')
+        return True
+        
     except ValueError:
         print('Formato de data inválido!')
         #raise ValueError("Incorrect data format, should be DD-MM-AAAA")
+        return False
 
-
-validate('23/12/2003')
 
 data = input('informe a data no formato DD/MM/AAAA:')
-dia = int(data[0:2])
-mes = int(data[3:5])
-ano = int(data[6:10])
 
-print(f'{dia} de {meses[mes]} de {ano}')
+if validate(data) == True:
+    dia = int(data[0:2])
+    mes = int(data[3:5])
+    ano = int(data[6:10])
+    print(f'{dia} de {meses[mes]} de {ano}')
+else:
+    print('Tente novamente')
+
+
